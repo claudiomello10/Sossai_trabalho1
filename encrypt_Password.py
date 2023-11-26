cbc8670637a7f850069bc100232d4a2ab5ce945a
202007140012 <claudio.mello@itec.ufpa.br>
2023-11-26 18:05:39 -0300

    dasd

--- encrypt_Password.py
+++ encrypt_Password.py
@@ -7,7 +7,6 @@ with open("password.txt", "r") as file:
 # Encrypt the password using hashlib
 hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-
 # Save the encrypted password in encryptedPassword.txt
 with open("encryptedPassword.txt", "w") as file:
     file.write(hashed_password)
