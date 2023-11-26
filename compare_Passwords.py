5f777f0ae3501bf2f85afc2fef93afc65eaf518d
202007140012 <claudio.mello@itec.ufpa.br>
2023-11-26 17:42:18 -0300

    first

--- compare_Passwords.py
+++ compare_Passwords.py
@@ -18,7 +18,12 @@ def main():
 
     if compare_passwords(password, encrypted_password):
         print("The passwords match.")
+        return True
+    else:
+        print("The passwords do not match.")
+        return False
 
 
 if __name__ == "__main__":
-    main()
+    result = main()
+    sys.exit(0 if result else 1)
